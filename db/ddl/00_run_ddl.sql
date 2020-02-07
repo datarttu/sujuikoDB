b98accbd4743e96515d5903abdd5405705a3ad3c
keripukki <arttu.kosonen@gmail.com>
2020-02-07 10:35:04 +0200

    Remove timing

--- db/ddl/00_run_ddl.sql
+++ db/ddl/00_run_ddl.sql
@@ -4,7 +4,6 @@ Create sujuiko database and execute DDL scripts from scratch.
 Arttu K 2020-02
 */
 \set ON_ERROR_STOP on
-\timing on
 
 CREATE DATABASE sujuiko;
 
@@ -15,5 +14,3 @@ BEGIN;
 \i 04_schedule_schema.sql
 \i 05_observation_schema.sql
 COMMIT;
-
-\timing off
