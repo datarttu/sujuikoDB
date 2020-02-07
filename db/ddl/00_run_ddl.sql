52639265f8185360f0be2300e998127956e27e23
keripukki <arttu.kosonen@gmail.com>
2020-02-07 10:34:29 +0200

    Wrap schema DDL into transaction

--- db/ddl/00_run_ddl.sql
+++ db/ddl/00_run_ddl.sql
@@ -8,10 +8,12 @@ Arttu K 2020-02
 
 CREATE DATABASE sujuiko;
 
+BEGIN;
 \i 01_extensions_global.sql
 \i 02_stage_schema.sql
 \i 03_network_schema.sql
 \i 04_schedule_schema.sql
 \i 05_observation_schema.sql
+COMMIT;
 
 \timing off
