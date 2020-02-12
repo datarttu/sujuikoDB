79ed2dfe5088e74f5232a578048f352941feab94
keripukki <arttu.kosonen@gmail.com>
2020-02-12 16:11:12 +0200

    Add raw hfp csv table

--- db/ddl/02_stage_schema.sql
+++ db/ddl/02_stage_schema.sql
@@ -3,11 +3,11 @@ Create tables for the staging schema.
 
 Arttu K 2020-02
 */
+\set ON_ERROR_STOP on
 \c sujuiko;
 
-CREATE SCHEMA IF NOT EXISTS stage_osm;
-CREATE SCHEMA IF NOT EXISTS stage_gtfs;
-CREATE SCHEMA IF NOT EXISTS stage_hfp;
+--CREATE SCHEMA IF NOT EXISTS stage_osm;
+--CREATE SCHEMA IF NOT EXISTS stage_gtfs;
 
 /*
 TODO: GTFS, OSM and HFP tables.
@@ -20,3 +20,26 @@ As for raw data:
   when importing data.
 - HFP: use pre-defined tables
 */
+BEGIN;
+
+CREATE SCHEMA IF NOT EXISTS stage_hfp;
+
+CREATE TABLE stage_hfp.raw (
+  is_ongoing    boolean,
+  event_type    text,
+  dir           smallint,
+  oper          smallint,
+  veh           integer,
+  tst           timestamptz,
+  lat           real,
+  lon           real,
+  odo           integer,
+  drst          boolean,
+  oday          date,
+  start         interval,
+  loc           text,
+  stop          integer,
+  route         text
+);
+
+COMMIT;
