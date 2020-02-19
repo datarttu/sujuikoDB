f57a60e2ece6473f98502ce5ce483dc14c07bead
keripukki <arttu.kosonen@gmail.com>
2020-02-19 17:17:13 +0200

    Update schedule schema

--- db/ddl/04_schedule_schema.sql
+++ db/ddl/04_schedule_schema.sql
@@ -5,8 +5,10 @@
  *
  * Arttu K 2020-02
  */
+\set ON_ERROR_STOP on
 \c sujuiko;
 
+BEGIN;
 CREATE SCHEMA IF NOT EXISTS sched;
 
 /*
@@ -18,36 +20,63 @@ CREATE SCHEMA IF NOT EXISTS sched;
  */
 CREATE TYPE sched.timing_type AS ENUM('strict', 'approx', 'interp');
 
-CREATE TABLE sched.services (
-  serv       text              NOT NULL,
-  date       date              NOT NULL,
-  PRIMARY KEY (serv, date)
-);
-
 CREATE TABLE sched.routes (
   route      text              PRIMARY KEY,
   mode       public.mode_type  NOT NULL
 );
 
-CREATE TABLE sched.trips (
-  tripid     text              PRIMARY KEY,
-  serv       text              NOT NULL REFERENCES sched.services(serv),
-  route      text              NOT NULL REFERENCES sched.routes(route),
-  dir        smallint          NOT NULL
+-- TODO: Also add buggy "whitespace + digit" route variants
+CREATE FUNCTION sched.populate_routes_from_gtfs()
+RETURNS TABLE (
+  mode            public.mode_type,
+  rows_inserted   bigint
+)
+LANGUAGE PLPGSQL
+VOLATILE
+AS $$
+BEGIN
+  RETURN QUERY
+  WITH
+    inserted AS (
+      INSERT INTO sched.routes
+      SELECT
+        route_id AS route,
+        (CASE
+         WHEN route_type = 0 THEN 'tram'
+         WHEN route_type IN (700, 701, 702, 704) THEN 'bus'
+         END
+        )::public.mode_type AS mode
+      FROM stage_gtfs.routes
+      ON CONFLICT DO NOTHING
+      RETURNING *
+    )
+  SELECT i.mode, count(i.route)::bigint AS rows_inserted
+  FROM inserted AS i
+  GROUP BY i.mode
+  ORDER BY i.mode;
+END;
+$$;
+
+CREATE TABLE sched.trip_templates (
+  tripid      text              PRIMARY KEY,
+  route       text              NOT NULL REFERENCES sched.routes(route),
+  dir         smallint          NOT NULL,
+  start_hms   interval          NOT NULL,
+  dates       date[]            NOT NULL
 );
-CREATE INDEX trips_serv_idx
-  ON sched.trips (serv);
 CREATE INDEX trips_route_dir_idx
-  ON sched.trips (route, dir);
+  ON sched.trip_templates (route, dir);
 
 CREATE TABLE sched.segments (
-  tripid         text                NOT NULL REFERENCES sched.trips(tripid),
-  inode          integer             NOT NULL,
-  jnode          integer             NOT NULL,
-  enter_hms      interval            NOT NULL,
+  tripid         text           NOT NULL REFERENCES sched.trip_templates(tripid),
+  inode          integer        NOT NULL,
+  jnode          integer        NOT NULL,
+  enter_hms      interval       NOT NULL,
   exit_hms       interval,
   enter_timing   sched.timing_type,
   exit_timing    sched.timing_type,
   PRIMARY KEY (tripid, inode, jnode, enter_hms),
   FOREIGN KEY (inode, jnode) REFERENCES nw.links(inode, jnode)
 );
+
+COMMIT;
