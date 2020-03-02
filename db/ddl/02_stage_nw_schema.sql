c5210922948fa084b0953abaa0b3e5635ad76eeb
keripukki <arttu.kosonen@gmail.com>
2020-03-02 13:18:06 +0200

    Contraction WIP

--- db/ddl/02_stage_nw_schema.sql
+++ db/ddl/02_stage_nw_schema.sql
@@ -75,4 +75,142 @@ BEGIN
 END;
 $$;
 
+CREATE OR REPLACE FUNCTION stage_nw.build_contracted_network()
+RETURNS TEXT
+LANGUAGE PLPGSQL
+VOLATILE
+AS $$
+BEGIN
+  RAISE NOTICE 'Building contracted network from stage_nw.raw_nw ...';
+  /*
+   * When merging edges, ignore one-way edge pairs that use the same
+   * source and target nodes but in opposite directions:
+   * this way they do not get merged.
+   */
+  RAISE NOTICE '  Creating temporary table for restricted edges ...';
+  DROP TABLE IF EXISTS stage_nw.restricted_edges;
+  CREATE TEMPORARY TABLE restricted_edges ON COMMIT DROP AS (
+    SELECT
+      a.id, a.source, a.target
+    FROM stage_nw.raw_nw        AS a
+    INNER JOIN stage_nw.raw_nw  AS b
+    ON  ((a.source = b.target AND a.target = b.source)
+        OR (a.id <> b.id AND a.source = b.source AND a.target = b.target))
+    ORDER BY a.id
+  );
+  RAISE NOTICE '  Creating table for contracted vertices arrays ...';
+  DROP TABLE IF EXISTS stage_nw.contracted_arr;
+  CREATE TABLE stage_nw.contracted_arr AS (
+    SELECT id, contracted_vertices, source, target, cost
+    FROM pgr_contraction(
+      'SELECT id, source, target, cost, reverse_cost
+       FROM stage_nw.raw_nw
+       WHERE id NOT IN (SELECT id FROM restricted_edges AS r)',
+       ARRAY[2], -- _linear_ contraction (2), as opposed to dead-end (1)
+       max_cycles := 1,
+       directed := true
+    )
+  );
+
+  RAISE NOTICE '  Creating table for contracted edges to merge ...';
+  DROP TABLE IF EXISTS stage_nw.contracted_edges_to_merge;
+  CREATE TABLE stage_nw.contracted_edges_to_merge AS (
+    WITH
+      /*
+       * Two-way links are represented twice in contraction arrays,
+       * therefore we only pick distinct ones.
+       */
+      distinct_contraction_arrays AS (
+        SELECT DISTINCT ON (contracted_vertices) *
+        FROM stage_nw.contracted_arr
+      ),
+      /*
+       * Include first and last vertices in the arrays,
+       * and open them up into rows so we get directly to vertex ids.
+       */
+      unnested AS (
+        SELECT
+          id AS grp,
+          unnest(source || contracted_vertices || target) AS vertex
+        FROM distinct_contraction_arrays
+      ),
+      /*
+       * Prepare all relevant pairs of listed vertices
+       * within each contraction group;
+       * next we find which of these pairs really form a link
+       * in the raw network table.
+       * This step is necessary because vertices in the contraction arrays
+       * are NOT ordered like they appear on the network.
+       */
+      vertex_permutations_by_grp AS (
+        SELECT u1.grp AS grp, u1.vertex AS source, u2.vertex AS target
+        FROM unnested AS u1, unnested AS u2
+        WHERE u1.grp = u2.grp AND u1.vertex <> u2.vertex
+      )
+    SELECT n.id, vp.grp, n.source, n.target
+    FROM stage_nw.raw_nw AS n
+    INNER JOIN vertex_permutations_by_grp AS vp
+    ON n.source = vp.source AND n.target = vp.target
+    ORDER BY vp.grp, n.id
+  );
+
+  RAISE NOTICE '  Creating contracted network edge table ...';
+  DROP TABLE IF EXISTS stage_nw.contracted_nw;
+  CREATE TABLE stage_nw.contracted_nw AS (
+    WITH
+      all_edges_before_merging AS (
+        SELECT
+          raw.id,
+          raw.source,
+          raw.target,
+          raw.oneway,
+          raw.mode,
+          coalesce(ctr.grp, raw.id) AS merge_group,
+          raw.geom
+        FROM stage_nw.raw_nw AS raw
+        LEFT JOIN stage_nw.contracted_edges_to_merge AS ctr
+        ON raw.source = ctr.source AND raw.target = ctr.target
+      )
+    SELECT
+      merge_group::bigint             AS id,
+      NULL::bigint                    AS source,
+      NULL::bigint                    AS target,
+      min(oneway)                     AS oneway,
+      min(mode)                       AS mode,
+      ST_LineMerge(ST_Collect(geom))  AS geom,
+      false                           AS is_contracted
+    FROM all_edges_before_merging
+    GROUP BY merge_group
+  );
+
+  RAISE NOTICE '  Updating contracted network edge ids ...';
+  /*
+   * Replace negative ids produced by the contraction routine.
+   * We rely on the fact that the least id used by OSM-based edges
+   * is > 2,000,000, and there are just thousands of contracted edges,
+   * so we just flip the negative id sign.
+   */
+  UPDATE stage_nw.contracted_nw
+  SET
+    id = abs(id),
+    is_contracted = true
+  WHERE id < 0;
+
+  RAISE NOTICE '  Creating pgr topology on contracted network ...';
+  PERFORM pgr_createTopology(
+    'stage_nw.contracted_nw',
+    1.0,
+    the_geom := 'geom',
+    id := 'id',
+    source := 'source',
+    target := 'target',
+    rows_where := 'true',
+    clean := true
+  );
+
+  RETURN 'OK';
+
+END;
+$$;
+
 COMMIT;
