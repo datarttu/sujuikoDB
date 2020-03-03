b29fb3f1a4129f7de24c3cabc790e4300e0b94c1
keripukki <arttu.kosonen@gmail.com>
2020-03-03 12:13:05 +0200

    Add patch for roundabout direction fixing

---
+++ db/patches/after_osm_import.sql
@@ -0,0 +1,16 @@
+/*
+ * Corrections on raw OSM data after importing.
+ */
+\set ON_ERROR_STOP on
+\c sujuiko;
+
+BEGIN;
+
+/*
+ * Fix roundabouts that have been modeled as two-way.
+ */
+UPDATE stage_osm.raw_bus_lines
+SET oneway = 'yes'
+WHERE junction LIKE 'roundabout';
+
+COMMIT;
