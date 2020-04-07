0e1df4720440666f9379df3056c0ea02c6d8c8ba
keripukki <arttu.kosonen@gmail.com>
2020-04-07 15:59:59 +0300

    Add working version of populate successive stops

--- db/ddl/02_stage_gtfs_schema.sql
+++ db/ddl/02_stage_gtfs_schema.sql
@@ -187,39 +187,37 @@ for finding network routes between stops.
 Based on ALL bus and tram stops found in the GTFS data.';
 
 CREATE OR REPLACE FUNCTION stage_gtfs.populate_successive_stops()
-RETURNS TABLE (
-  mode            public.mode_type,
-  rows_inserted   bigint
-)
+RETURNS TEXT
 LANGUAGE PLPGSQL
 VOLATILE
 AS $$
+DECLARE
+  cnt   integer;
 BEGIN
-  IF EXISTS (
-    SELECT *
-    FROM stage_gtfs.successive_stops
-    LIMIT 1
-  ) THEN
-    RAISE EXCEPTION 'Table stage_gtfs.successive_stops is not empty!'
-    USING HINT = 'Truncate the table first.';
-  END IF;
+  DELETE FROM stage_gtfs.successive_stops;
+  GET DIAGNOSTICS cnt = ROW_COUNT;
+  RAISE NOTICE '% rows deleted from stage_gtfs.successive_stops', cnt;
 
-  RETURN QUERY
-  WITH
-    successive_all AS (
-      -- TODO: Make filtered table for bus & tram stop times first!
-    ),
-    successive_inserted AS (
-      INSERT INTO stage_gtfs.successive_stops
-      SELECT DISTINCT mode, i_stop, j_stop
-      FROM successive_all
-      WHERE j_stop IS NOT NULL
-      RETURNING *
-    )
-  SELECT
-    mode, count(mode)::bigint AS rows_inserted
-  FROM successive_inserted;
+  INSERT INTO stage_gtfs.successive_stops
+  SELECT DISTINCT
+    a.stop_id   AS i_stop,
+    c.stop_id   AS j_stop
+  FROM stage_gtfs.stop_times            AS a
+  INNER JOIN stage_gtfs.stops_with_mode AS b
+    ON  a.stop_id       = b.stopid
+  INNER JOIN stage_gtfs.stop_times      AS c
+    ON  a.trip_id       = c.trip_id
+    AND a.stop_sequence = (c.stop_sequence - 1);
+  GET DIAGNOSTICS cnt = ROW_COUNT;
+  RAISE NOTICE '% stop pairs inserted into stage_gtfs.successive_stops', cnt;
+
+  RETURN 'OK';
 END;
 $$;
+COMMENT ON FUNCTION stage_gtfs.populate_successive_stops IS
+'Find unique successive stop pairs
+from tram & bus stop times
+and insert them into stage_gtfs.successive_stops table.
+stage_gtfs.successive_stops is emptied first.';
 
 COMMIT;
