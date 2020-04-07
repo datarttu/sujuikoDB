c6e275dc053d4ebfcb52f25a6c3ae1150f12ee19
keripukki <arttu.kosonen@gmail.com>
2020-04-07 12:49:39 +0300

    Add create node table function

--- db/ddl/03_network_schema.sql
+++ db/ddl/03_network_schema.sql
@@ -31,6 +31,44 @@ CREATE INDEX links_wgs_geom_idx
 CREATE INDEX links_mode_idx
   ON nw.links (mode);
 
+CREATE OR REPLACE FUNCTION nw.create_node_table()
+RETURNS TEXT
+LANGUAGE PLPGSQL
+VOLATILE
+AS $$
+DECLARE
+  cnt   integer;
+BEGIN
+  PERFORM pgr_createTopology(
+    'nw.links',
+    0.01,
+    the_geom    := 'geom',
+    id          := 'linkid',
+    source      := 'inode',
+    target      := 'jnode',
+    rows_where  := 'true',
+    clean       := true
+  );
+  ALTER TABLE nw.links_vertices_pgr
+  RENAME TO nodes;
+  ALTER TABLE nw.nodes
+  RENAME COLUMN id TO nodeid;
+  ALTER TABLE nw.nodes
+  RENAME COLUMN the_geom TO geom;
+  ALTER TABLE nw.nodes
+  ADD COLUMN wgs_geom geometry(POINT, 4326);
+  UPDATE nw.nodes
+  SET wgs_geom = ST_Transform(geom, 4326);
+  GET DIAGNOSTICS cnt = ROW_COUNT;
+  RAISE NOTICE '% nodes created for nw.links', cnt;
+  RETURN 'OK';
+
+END;
+$$;
+COMMENT ON FUNCTION nw.create_node_table IS
+'Create nw.nodes table by building topology from nw.links
+and renaming table and columns created by pgr_createTopology.';
+
 CREATE TABLE nw.stops (
   stopid      integer           PRIMARY KEY,
   nodeid      integer           NOT NULL,
