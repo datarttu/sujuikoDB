0a08d0b9c69156cdf2d33fb6d77a532f934d0950
keripukki <arttu.kosonen@gmail.com>
2020-04-07 10:15:16 +0300

    Remove comment on separate links
    In fact, we will NOT handle two-way links as separate objects per 
    direction.

--- db/ddl/03_network_schema.sql
+++ db/ddl/03_network_schema.sql
@@ -24,13 +24,6 @@ CREATE INDEX nodes_wgs_geom_idx
   ON nw.nodes
   USING GIST (wgs_geom);
 
-/*
- * Even though OSM ways can be oneway or two-way,
- * we handle two-way streets as separate links here.
- * Thus the "reverse cost" options will be never used with pgrouting.
- * The two-way links should have the same geometry
- * except that the points are listed in reverse order.
- */
 CREATE TABLE nw.links (
   linkid        integer           PRIMARY KEY,
   inode         integer           REFERENCES nw.nodes (nodeid),
