59986e04d43ce3929740aca7ec1112107d17dbc3
keripukki <arttu.kosonen@gmail.com>
2020-04-08 10:22:41 +0300

    Move route function from sched to stage_gtfs

--- db/ddl/04_schedule_schema.sql
+++ db/ddl/04_schedule_schema.sql
@@ -25,37 +25,6 @@ CREATE TABLE sched.routes (
   mode       public.mode_type  NOT NULL
 );
 
-CREATE FUNCTION sched.populate_routes_from_gtfs()
-RETURNS TABLE (
-  mode            public.mode_type,
-  rows_inserted   bigint
-)
-LANGUAGE PLPGSQL
-VOLATILE
-AS $$
-BEGIN
-  RETURN QUERY
-  WITH
-    inserted AS (
-      INSERT INTO sched.routes
-      SELECT
-        route_id AS route,
-        (CASE
-         WHEN route_type = 0 THEN 'tram'
-         WHEN route_type IN (700, 701, 702, 704) THEN 'bus'
-         END
-        )::public.mode_type AS mode
-      FROM stage_gtfs.routes
-      ON CONFLICT DO NOTHING
-      RETURNING *
-    )
-  SELECT i.mode, count(i.route)::bigint AS rows_inserted
-  FROM inserted AS i
-  GROUP BY i.mode
-  ORDER BY i.mode;
-END;
-$$;
-
 CREATE TABLE sched.trip_templates (
   tripid      text              PRIMARY KEY,
   route       text              NOT NULL REFERENCES sched.routes(route),
