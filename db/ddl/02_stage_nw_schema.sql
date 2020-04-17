b5a4baca80758fcc3392299576b0b38b63293f36
keripukki <arttu.kosonen@gmail.com>
2020-04-17 11:48:16 +0300

    Add view for failed node pairs

--- db/ddl/02_stage_nw_schema.sql
+++ db/ddl/02_stage_nw_schema.sql
@@ -887,4 +887,15 @@ nw.nodes and nw.stops, using pgr_Dijkstra.
 Note that routes are not between stops but their respective NODES.
 Results are stored in stage_nw.node_pair_routes.';
 
+CREATE VIEW stage_nw.node_pairs_not_routed_geom AS (
+  SELECT a.i_node, a.j_node, ST_MakeLine(b.geom, c.geom) AS geom
+  FROM stage_nw.successive_nodes AS a
+  INNER JOIN nw.nodes AS b ON a.i_node = b.nodeid
+  INNER JOIN nw.nodes AS c ON a.j_node = c.nodeid
+  WHERE NOT a.routed
+);
+COMMENT ON VIEW stage_nw.node_pairs_not_routed_geom IS
+'Failed node pairs that have not marked as successfully routed,
+with line geometry for visualization.';
+
 COMMIT;
