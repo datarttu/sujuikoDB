9607215f7e89a4e56d5a1dd9dff65d72fd0beac4
keripukki <arttu.kosonen@gmail.com>
2020-05-06 16:44:28 +0300

    Trip template segment transfer WIP

---
+++ db/explore/prep_trip_templates.sql
@@ -0,0 +1,44 @@
+/*
+ * We make a significant set of modifications to the trip templates
+ * when transferring them to `sched` schema:
+ * -  Only valid and meaningful templates are transferred:
+ *    see the WHERE clause in the first part of the CTE.
+ * -  Stop times are only used if the stop is defined as timepoint.
+ *    Moreover, the first and last stop of the template is always set as timepoint.
+ *    Segment times, including non-timepoint stop times, will be interpolated
+ *    between these timepoint stops.
+ * -  Stop sequences are decomposed into route segments,
+ *    and segment values are calculated using "partitions".
+ *    A partition = route part between timepoint stops.
+ */
+
+WITH
+
+  tt_arr_unnested AS (
+    SELECT
+      ttid,
+      unnest(stop_sequences)  AS stop_seq,
+      unnest(arr_time_diffs)  AS arr,
+      unnest(dep_time_diffs)  AS dep,
+      unnest(timepoints)      AS timepoint
+    FROM stage_gtfs.trip_template_arrays
+    WHERE route_found IS true
+      AND start_times IS NOT NULL
+      AND dates IS NOT NULL
+      AND route_id = '1001' -- REMOVE THIS FILTER FOR PRODUCTION!!
+  ),
+
+  tt_set_firstlast_timepoints AS (
+    SELECT ttid, stop_seq, arr, dep,
+      CASE
+        WHEN stop_seq = 1 THEN true
+        WHEN stop_seq = max(stop_seq) OVER (PARTITION BY ttid) THEN true
+        ELSE timepoint
+      END AS timepoint
+    FROM tt_arr_unnested
+  )
+
+SELECT *
+FROM tt_set_firstlast_timepoints
+ORDER BY ttid, stop_seq
+LIMIT 60;
