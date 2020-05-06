d214c1c932733031ed8949484489733ac3a820e0
keripukki <arttu.kosonen@gmail.com>
2020-05-06 16:01:27 +0300

    Add table documentation

--- db/ddl/04_schedule_schema.sql
+++ db/ddl/04_schedule_schema.sql
@@ -50,5 +50,15 @@ CREATE TABLE sched.segments (
   j_rel_dist        double precision  NOT NULL CHECK (j_rel_dist BETWEEN 0 AND 1),
   PRIMARY KEY (ttid, linkid, enter_hms)
 );
+COMMENT ON TABLE sched.segments IS
+'Refers to links that, ordered by enter time `i_time`, comprise the route
+geometry of trip template `ttid`.
+Values at link enter point are prefixed with `i`, and values at exit point with `j`.
+`_time` values are relative to the trip template start, beginning from 0.
+`_stop`: true if the point is a stop in the template schedule,
+         false if it is a normal node.
+`_strict`: true if `_time` at that point is absolute and "planned",
+           false if it has been interpolated.
+`_rel_dist`: distance relative to the length of the entire trip template geometry.';
 
 COMMIT;
