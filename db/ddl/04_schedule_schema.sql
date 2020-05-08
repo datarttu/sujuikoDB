a6d60b21a55d066b1666deb5ccd7b14d221db60a
keripukki <arttu.kosonen@gmail.com>
2020-05-08 13:58:54 +0300

    Create view for individual trips

--- db/ddl/04_schedule_schema.sql
+++ db/ddl/04_schedule_schema.sql
@@ -52,4 +52,27 @@ Values at link enter point are prefixed with `i`, and values at exit point with 
            false if it has been interpolated.
 `_rel_dist`: distance relative to the length of the entire trip template geometry.';
 
+CREATE VIEW sched.individual_trips AS (
+  WITH
+    unnest_dates AS (
+     SELECT
+       ttid,
+       route,
+       dir,
+       start_times,
+       unnest(dates)  AS service_date
+     FROM sched.trip_templates
+    )
+  SELECT
+   ttid,
+   route,
+   dir,
+   service_date,
+   unnest(start_times) AS start_time
+  FROM unnest_dates
+);
+COMMENT ON VIEW sched.individual_trips IS
+'Opens up trip templates into individual trips
+with start times and service dates.';
+
 COMMIT;
