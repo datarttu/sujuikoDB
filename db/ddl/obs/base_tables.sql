f65afd6f901fdcc88dfeca6226f56301e0a54b13
keripukki <arttu.kosonen@gmail.com>
2020-05-08 20:39:15 +0300

    Update sched reference

--- db/ddl/obs/base_tables.sql
+++ db/ddl/obs/base_tables.sql
@@ -27,7 +27,7 @@ CREATE UNIQUE INDEX vehicles_oper_veh_idx
  */
 CREATE TABLE obs.journeys (
   start_ts   timestamptz      NOT NULL,
-  tripid     text             NOT NULL REFERENCES sched.trips(tripid),
+  ttid       text             NOT NULL REFERENCES sched.trip_templates(ttid),
   jrnid      uuid             NOT NULL, -- TODO: md5 trigger
   vehid      integer          NOT NULL REFERENCES obs.vehicles(vehid),
   PRIMARY KEY (start_ts, tripid)
