ffd20bb8f998020bd1ae49be18f0c3898ec401df
keripukki <arttu.kosonen@gmail.com>
2020-05-15 12:28:15 +0300

    Add function for fixing unconnected edges

--- db/ddl/stage_osm/combined_lines.sql
+++ db/ddl/stage_osm/combined_lines.sql
@@ -84,3 +84,129 @@ BEGIN
   RETURN 'OK';
 END;
 $$;
+
+/*
+ * TODO: This function seems not to fix all the spots necessary, investigate!
+ */
+CREATE OR REPLACE FUNCTION stage_osm.fix_unconnected_combined_lines(
+  drop_intermediate_tables  boolean   DEFAULT true
+)
+RETURNS TEXT
+LANGUAGE PLPGSQL
+VOLATILE
+AS $$
+DECLARE
+  cnt     bigint;
+  s_info  text;
+BEGIN
+  DROP TABLE IF EXISTS stage_osm.unconnected_combined_lines;
+  CREATE TABLE stage_osm.unconnected_combined_lines AS (
+    SELECT
+      f.osm_id                                    AS failing_link,
+      t.osm_id                                    AS touching_link,
+      f.mode                                      AS mode,
+      f.geom                                      AS f_geom,
+      t.geom                                      AS t_geom
+    FROM stage_osm.combined_lines       AS f
+    INNER JOIN stage_osm.combined_lines AS t
+    /*
+     * Note: ST_Touches is significantly faster than ST_Relate and is therefore
+     *       used first to speed up the join. ST_Relate then narrows down
+     *       the join condition so that we don't end up with symmetric pairs
+     *       of failing and touching links.
+     */
+      ON ST_Touches(f.geom, t.geom)
+      AND ST_Relate(f.geom, t.geom, 'F01FF0102')
+      AND f.mode = t.mode
+  );
+  SELECT
+    'Failing links to split: '
+    || count(*) filter(WHERE mode = 'bus'::mode_type) || ' bus, '
+    || count(*) filter(WHERE mode = 'tram'::mode_type) || ' tram'
+  INTO s_info
+  FROM stage_osm.unconnected_combined_lines;
+
+  RAISE NOTICE '%', s_info;
+
+  DROP TABLE IF EXISTS stage_osm.split_combined_lines;
+  CREATE TABLE stage_osm.split_combined_lines AS (
+    WITH
+    all_split_points AS (
+      SELECT
+        failing_link,
+        touching_link,
+        f_geom,
+        (ST_Dump(
+          ST_Intersection(f_geom, t_geom)
+        )).geom AS pt_geom
+      FROM stage_osm.unconnected_combined_lines
+      ORDER BY failing_link, touching_link
+    ),
+    distinct_split_points AS (
+      SELECT DISTINCT ON (pt_geom) *
+      FROM all_split_points
+    ),
+    multi_split_points AS (
+      SELECT
+        failing_link,
+        f_geom,
+        ST_Collect(pt_geom) AS pts_geom
+      FROM distinct_split_points
+      GROUP BY failing_link, f_geom
+    ),
+    split AS (
+      SELECT
+        failing_link,
+        f_geom,
+        (ST_Dump(
+          ST_Split(f_geom, pts_geom)
+        )).geom AS sp_geom
+      FROM multi_split_points
+    )
+    SELECT
+      failing_link,
+      row_number() OVER (
+        PARTITION BY failing_link) AS sub_id,
+      f_geom,
+      sp_geom
+    FROM split
+  );
+
+  INSERT INTO stage_osm.combined_lines (
+    osm_id, sub_id, oneway, mode, highway, lanes, tram_segregation_physical, geom
+  )
+  SELECT
+    scl.failing_link              AS osm_id,
+    scl.sub_id                    AS sub_id,
+    cl.oneway                     AS oneway,
+    cl.mode                       AS mode,
+    cl.highway                    AS highway,
+    cl.lanes                      AS lanes,
+    cl.tram_segregation_physical  AS tram_segregation_physical,
+    scl.sp_geom                   AS geom
+  FROM stage_osm.split_combined_lines   AS scl
+  INNER JOIN stage_osm.combined_lines   AS cl
+    ON scl.failing_link = cl.osm_id
+  ON CONFLICT ON CONSTRAINT combined_lines_pkey DO UPDATE
+    SET geom = EXCLUDED.geom;
+
+  GET DIAGNOSTICS cnt = ROW_COUNT;
+  RAISE NOTICE '% rows inserted or updated', cnt;
+
+  IF drop_intermediate_tables THEN
+    RAISE NOTICE 'Dropping intermediate tables';
+    DROP TABLE stage_osm.unconnected_combined_lines;
+    DROP TABLE stage_osm.split_combined_lines;
+  END IF;
+
+  RETURN 'OK';
+END;
+$$;
+COMMENT ON FUNCTION stage_osm.fix_unconnected_combined_lines IS
+'Fix assumed topology errors in stage_osm.combined_lines
+where the end of link A touches the face of link B,
+while in fact link B should be split on that point.
+Links of type B are split and they are assigned a sub_id;
+split part with original sub_id is updated, others inserted as new records.
+NOTE: This is assumed a one-time operation, such that in the beginning
+all the sub_ids have the default value of 1.';
