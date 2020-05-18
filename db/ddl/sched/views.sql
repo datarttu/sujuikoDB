0fcb306b2e2e39e97735b47b28fbd883e56a55dd
keripukki <arttu.kosonen@gmail.com>
2020-05-18 17:07:47 +0300

    Add view on individual trip segments

--- db/ddl/sched/views.sql
+++ db/ddl/sched/views.sql
@@ -23,3 +23,21 @@ CREATE VIEW sched.view_trips AS (
 COMMENT ON VIEW sched.view_trips IS
 'Opens up trip templates into individual trips
 with actual start datetimes.';
+
+CREATE VIEW sched.view_trip_segments AS (
+  SELECT
+    tp.ttid,
+    tp.route,
+    tp.dir,
+    tp.service_date,
+    tp.start_time,
+    tp.start_ts,
+    sg.linkid,
+    sg.i_node,
+    sg.j_node,
+    tp.start_ts + sg.i_time AS i_ts,
+    tp.start_ts + sg.j_time AS j_ts
+  FROM sched.view_trips     AS tp
+  INNER JOIN sched.segments AS sg
+    ON tp.ttid = sg.ttid
+);
