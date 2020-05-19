bdc983414191c59062ce1d75a1752965c9e0f570
keripukki <arttu.kosonen@gmail.com>
2020-05-19 09:47:32 +0300

    Add md5 tripid field

--- db/ddl/sched/views.sql
+++ db/ddl/sched/views.sql
@@ -9,21 +9,33 @@ CREATE VIEW sched.view_trips AS (
        start_times,
        unnest(dates)  AS service_date
      FROM sched.trip_templates
+    ),
+    unnest_starttimes AS (
+      SELECT
+        *,
+        unnest(start_times) AS start_time
+      FROM unnest_dates
     )
   SELECT
-   ttid,
-   route,
-   dir,
-   service_date,
-   unnest(start_times)      AS start_time,
-   (service_date
+    ttid,
+    md5(
+      concat_ws(
+        '_',
+        service_date, start_time, route, dir
+      )
+    )::uuid         AS tripid,
+    route,
+    dir,
+    service_date,
+    start_time,
+    (service_date
      || ' Europe/Helsinki')::timestamptz
-     + unnest(start_times)  AS start_ts
-  FROM unnest_dates
+     + start_time   AS start_ts
+  FROM unnest_starttimes
 );
 COMMENT ON VIEW sched.view_trips IS
 'Opens up trip templates into individual trips
-with actual start datetimes.';
+with unique trip ids and actual start datetimes.';
 
 DROP VIEW IF EXISTS sched.view_trip_segments;
 CREATE VIEW sched.view_trip_segments AS (
