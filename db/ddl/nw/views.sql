53f728ad42478396545869dba158c0a23893f43a
keripukki <arttu.kosonen@gmail.com>
2020-05-19 14:41:47 +0300

    Add view on links with reverse records added

---
+++ db/ddl/nw/views.sql
@@ -0,0 +1,25 @@
+CREATE VIEW nw.view_links_with_reverses AS (
+  SELECT
+    linkid, inode, jnode, mode, cost, rcost, osm_data, geom, wgs_geom,
+    false AS reversed
+  FROM nw.links
+  UNION
+  SELECT
+    linkid,
+    jnode                 AS inode,
+    inode                 AS jnode,
+    mode,
+    rcost                 AS cost,
+    cost                  AS rcost,
+    osm_data,
+    ST_Reverse(geom)      AS geom,
+    ST_Reverse(wgs_geom)  AS wgs_geom,
+    true                  AS reversed
+  FROM  nw.links
+  WHERE rcost > -1
+);
+COMMENT ON VIEW nw.view_links_with_reverses IS
+'All nw.links records plus separate records for two-way links,
+created by inverting inode and jnode, cost and rcost, and geoms.
+Records should be unique and thus safely joinable on
+by linkid, inode and jnode.';
