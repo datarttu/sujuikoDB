99c135fea310feed98cb33fbfeecc2203f1fc5d9
keripukki <arttu.kosonen@gmail.com>
2020-06-11 14:01:15 +0300

    Add = default initialization

--- db/ddl/stage_gtfs/run_patterns.sql
+++ db/ddl/stage_gtfs/run_patterns.sql
@@ -6,6 +6,9 @@ SELECT * FROM stage_gtfs.set_pattern_paths();
 SELECT * FROM stage_gtfs.set_pattern_stops_path_found();
 SELECT * FROM stage_gtfs.set_patterns_length_values();
 
+UPDATE stage_gtfs.pattern_stops SET invalid_reasons = DEFAULT;
+UPDATE stage_gtfs.patterns SET invalid_reasons = DEFAULT;
+
 SELECT * FROM invalidate('stage_gtfs.pattern_stops', 'No network path', 'path_found IS false');
 SELECT * FROM invalidate('stage_gtfs.pattern_stops', 'Network path too long', 'nw_vs_shape_coeff > 1.2');
 SELECT * FROM invalidate('stage_gtfs.pattern_stops', 'Network path too short', 'nw_vs_shape_coeff < 0.8');
