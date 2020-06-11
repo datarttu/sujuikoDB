6f146c2c497cbbf0dabc18096395db0d805929b7
keripukki <arttu.kosonen@gmail.com>
2020-06-11 13:47:00 +0300

    Add script for running pattern-related staging stuff at once

---
+++ db/ddl/stage_gtfs/run_patterns.sql
@@ -0,0 +1,13 @@
+SELECT * FROM stage_gtfs.extract_trip_stop_patterns();
+SELECT * FROM stage_gtfs.set_pattern_stops_shape_geoms();
+SELECT * FROM stage_gtfs.extract_unique_stop_pairs();
+SELECT * FROM stage_gtfs.find_stop_pair_paths();
+SELECT * FROM stage_gtfs.set_pattern_paths();
+SELECT * FROM stage_gtfs.set_pattern_stops_path_found();
+SELECT * FROM stage_gtfs.set_patterns_length_values();
+
+SELECT * FROM invalidate('stage_gtfs.pattern_stops', 'Network path too long', 'nw_vs_shape_coeff > 1.2');
+SELECT * FROM invalidate('stage_gtfs.pattern_stops', 'Network path too short', 'nw_vs_shape_coeff < 0.8');
+SELECT * FROM stage_gtfs.count_pattern_stops_invalidations();
+SELECT * FROM invalidate('stage_gtfs.patterns', 'Network path too long', 'nw_vs_shape_coeff > 1.1');
+SELECT * FROM invalidate('stage_gtfs.patterns', 'Network path too short', 'nw_vs_shape_coeff < 0.9');
