c30401b346bbb0507719b2bf26c0922a02135a72
keripukki <arttu.kosonen@gmail.com>
2020-06-26 12:52:53 +0300

    Drop old segment views

--- db/ddl/sched/views.sql
+++ db/ddl/sched/views.sql
@@ -18,55 +18,6 @@ COMMENT ON VIEW sched.view_trips IS
 'Opens up trip templates into individual trips
 with unique trip ids and actual start datetimes.';
 
-DROP VIEW IF EXISTS sched.view_trip_segments CASCADE;
-CREATE VIEW sched.view_trip_segments AS (
-  SELECT
-    tp.ttid,
-    tp.route,
-    tp.dir,
-    tp.service_date,
-    tp.start_time,
-    tp.start_ts,
-    sg.linkid,
-    sg.i_node,
-    sg.j_node,
-    sg.i_stop,
-    sg.j_stop,
-    tp.start_ts + sg.i_time AS i_ts,
-    tp.start_ts + sg.j_time AS j_ts
-  FROM sched.view_trips     AS tp
-  INNER JOIN sched.segments AS sg
-    ON tp.ttid = sg.ttid
-);
-COMMENT ON VIEW sched.view_trip_segments IS
-'Segments of individual trips,
-with absolute link enter (i) and exit (j) timestamps.';
-
-DROP VIEW IF EXISTS sched.view_segment_geoms CASCADE;
-CREATE VIEW sched.view_segment_geoms AS (
-  SELECT
-    sg.ttid,
-    sg.linkid,
-    sg.i_node,
-    sg.j_node,
-    sg.i_stop,
-    sg.j_stop,
-    sg.i_time,
-    li.cost,
-    sum(li.cost) OVER (PARTITION BY sg.ttid ORDER BY sg.i_time) - li.cost AS i_cumul_cost,
-    li.reversed,
-    li.geom
-  FROM sched.segments                     AS sg
-  INNER JOIN nw.view_links_with_reverses  AS li
-    ON  sg.linkid = li.linkid
-    AND sg.i_node = li.inode
-    AND sg.j_node = li.jnode
-);
-COMMENT ON VIEW sched.view_segment_geoms IS
-'Trip template segments with their respective link geometries.
-"reversed" indicates if a reversed two-way link geometry is referenced,
-by inverting i and j.';
-
 DROP MATERIALIZED VIEW IF EXISTS sched.mw_trip_template_geoms CASCADE;
 CREATE MATERIALIZED VIEW sched.mw_trip_template_geoms AS (
   SELECT
