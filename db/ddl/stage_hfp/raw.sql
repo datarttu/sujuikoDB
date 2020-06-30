c0e65661783827dddad66fcfba81fd249bf4689f
keripukki <arttu.kosonen@gmail.com>
2020-06-30 17:10:39 +0300

    Add insert blocker trigger for non-ongoing non-VP rows

--- db/ddl/stage_hfp/raw.sql
+++ db/ddl/stage_hfp/raw.sql
@@ -23,6 +23,28 @@ CREATE INDEX ON stage_hfp.raw USING BTREE (jrnid, tst);
 CREATE INDEX ON stage_hfp.raw USING BTREE (start_ts, route, dir);
 CREATE INDEX ON stage_hfp.raw USING GIST (geom);
 
+CREATE FUNCTION stage_hfp.ignore_inserts()
+RETURNS trigger
+LANGUAGE PLPGSQL
+AS $$
+BEGIN
+  IF
+    NEW.is_ongoing IS true
+    AND NEW.event_type = 'VP'
+  THEN RETURN NEW;
+  ELSE RETURN NULL;
+  END IF;
+END;
+$$;
+COMMENT ON FUNCTION stage_hfp.ignore_inserts() IS
+'Blocks from insertion any row where is_ongoing is not true and / or
+event_type is other than VP.';
+
+CREATE TRIGGER aaa_ignore_inserts
+BEFORE INSERT ON stage_hfp.raw
+FOR EACH ROW
+EXECUTE PROCEDURE stage_hfp.ignore_inserts();
+
 CREATE FUNCTION stage_hfp.set_raw_additional_fields()
 RETURNS trigger
 LANGUAGE PLPGSQL
