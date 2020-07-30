d0857c934c38f15fea898d2a443c20e8ee1b613c
keripukki <arttu.kosonen@gmail.com>
2020-07-30 11:50:25 +0300

    Add local run_ddl script

---
+++ db/ddl/stage_hfp/run_ddl.sql
@@ -0,0 +1,13 @@
+/*
+ * stage_hfp DDL scripts.
+ * In production, you can use main level RUN_DDL.sql.
+ * This leaves the transaction open so you must commit manually if needed!
+ */
+
+BEGIN;
+
+\ir raw.sql
+\ir journeys.sql
+\ir logging.sql
+\ir jrn_points.sql
+\ir jrn_segs.sql
