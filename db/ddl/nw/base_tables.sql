6ff9b4013e8c344eb15d402ddfc6365f5798f873
keripukki <arttu.kosonen@gmail.com>
2020-09-15 00:12:32 +0300

    Add link inode & jnode location checks

--- db/ddl/nw/base_tables.sql
+++ db/ddl/nw/base_tables.sql
@@ -74,13 +74,51 @@ CREATE TABLE nw.links (
   jnode         integer             NOT NULL REFERENCES nw.nodes(nodeid),
   mode          text                NOT NULL CHECK (mode IN ('bus', 'tram')),
   oneway        boolean             NOT NULL,
-  cost          double precision,
-  rcost         double precision    GENERATED ALWAYS AS (CASE WHEN oneway THEN -1 ELSE cost END) STORED,
+  cost          double precision    GENERATED ALWAYS AS (ST_Length(geom)) STORED,
+  rcost         double precision    GENERATED ALWAYS AS (CASE WHEN oneway THEN -1 ELSE ST_Length(geom) END) STORED,
   attributes    jsonb,
-  geom          geometry(LINESTRING, 3067)
+  geom          geometry(LINESTRING, 3067)  NOT NULL
 );
+COMMENT ON TABLE nw.links IS
+'Represent connections between nodes.';
+
 CREATE INDEX ON nw.links (mode);
 CREATE INDEX ON nw.links USING GIST (geom);
+
+-- inode and jnode location checks
+-- These apply only if you try to modify inode / jnode directly.
+-- When inserting a new link without existing nodes, they will be created.
+CREATE FUNCTION nw.validate_link_node_references()
+RETURNS trigger
+LANGUAGE PLPGSQL
+AS $$
+DECLARE
+  node_geom     geometry(POINT, 3067);
+BEGIN
+  SELECT INTO node_geom geom FROM nw.nodes WHERE nodeid = NEW.inode;
+  IF node_geom IS NULL THEN
+    RAISE WARNING 'inode % of link % does not exist, link not updated', NEW.inode, NEW.linkid;
+  ELSIF NOT ST_StartPoint(NEW.geom) && node_geom THEN
+    RAISE WARNING 'Link % geom does not start from inode %, link not updated', NEW.linkid, NEW.inode;
+    RETURN NULL;
+  END IF;
+  SELECT INTO node_geom geom FROM nw.nodes WHERE nodeid = NEW.jnode;
+  IF node_geom IS NULL THEN
+    RAISE WARNING 'jnode % of link % does not exist, link not updated', NEW.jnode, NEW.linkid;
+  ELSIF NOT ST_EndPoint(NEW.geom) && node_geom THEN
+    RAISE WARNING 'Link % geom does not end to jnode %, link not updated', NEW.linkid, NEW.jnode;
+    RETURN NULL;
+  END IF;
+END;
+$$;
+COMMENT ON FUNCTION nw.validate_link_node_references() IS
+'Ensures that the `inode` and `jnode` of the link not only exist in the node table
+but their respective geometries also point to the link start and end.';
+
+CREATE TRIGGER validate_link_node_references
+BEFORE UPDATE OF inode, jnode ON nw.links
+FOR EACH ROW EXECUTE PROCEDURE nw.validate_link_node_references();
+
 
 
 CREATE TABLE nw.stops (
--- db/mock_test.sql
+++ db/mock_test.sql
@@ -1,8 +1,20 @@
--- Should be a valid node:
-INSERT INTO nw.nodes(geom) VALUES (ST_SetSRID(ST_MakePoint(385443, 6671474), 3067));
+\qecho Should be valid nodes:
+INSERT INTO nw.nodes(nodeid, geom) VALUES (1, ST_SetSRID(ST_MakePoint(385443, 6671474), 3067));
+INSERT INTO nw.nodes(nodeid, geom) VALUES (2, ST_SetSRID(ST_MakePoint(385403, 6671479), 3067));
+INSERT INTO nw.nodes(nodeid, geom) VALUES (3, ST_SetSRID(ST_MakePoint(385103, 6671238), 3067));
 
--- Should be an INVALID node, outside HSL area:
+\qecho Should be an INVALID node, outside HSL area:
 INSERT INTO nw.nodes(geom) VALUES (ST_SetSRID(ST_MakePoint(226574, 6928728), 3067));
 
--- Should be an INVALID node, too close to an existing node:
+\qecho Should be an INVALID node, too close to an existing node:
 INSERT INTO nw.nodes(geom) VALUES (ST_SetSRID(ST_MakePoint(385443.5, 6671474.5), 3067));
+
+\qecho Should be a valid link:
+INSERT INTO nw.links(linkid, inode, jnode, mode, oneway, geom)
+  VALUES (1, 1, 2, 'bus', false, ST_SetSRID(ST_MakeLine(ST_MakePoint(385443, 6671474), ST_MakePoint(385403, 6671479)), 3067));
+
+\qecho Update should result in an INVALID link, because of inode mismatch:
+UPDATE nw.links SET inode = 3 WHERE linkid = 1;
+
+\qecho Update should result in an INVALID link, because of jnode mismatch:
+UPDATE nw.links SET jnode = 3 WHERE linkid = 1;
