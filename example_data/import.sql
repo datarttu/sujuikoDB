8771c8d9f5427319bc2eb196c1b78946e77820df
keripukki <arttu.kosonen@gmail.com>
2021-06-07 11:39:34 +0300

    Fix proc name

--- example_data/import.sql
+++ example_data/import.sql
@@ -3,7 +3,7 @@
 \copy nw.view_stop_wkt FROM '/data/stop.csv' CSV HEADER;
 \copy nw.route_version (route_ver_id, route, dir, valid_during, route_mode) FROM '/data/route_version.csv' CSV HEADER;
 \copy nw.stop_on_route (route_ver_id, stop_seq, stop_id, active_place) FROM '/data/stop_on_route.csv' CSV HEADER;
-CALL nw.update_stop_link_refs(20.0);
+CALL nw.batch_update_stop_link_refs(20.0);
 
 \copy nw.section (section_id, description, via_nodes) FROM '/data/section.csv' CSV HEADER;
 CALL nw.batch_upsert_links_on_section();
