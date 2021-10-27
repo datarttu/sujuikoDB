c14b3866c39a810d5c903f7d13dda74f86d6d493
datarttu <arttu.kosonen@gmail.com>
2021-10-27 15:47:08 +0300

    Update example data, remove nodes

--- example_data/import.sql
+++ example_data/import.sql
@@ -1,4 +1,3 @@
-\copy nw.view_node_wkt FROM '/data/node.csv' CSV HEADER;
 \copy nw.view_link_wkt FROM '/data/link.csv' CSV HEADER;
 \copy nw.view_stop_wkt FROM '/data/stop.csv' CSV HEADER;
 \copy nw.route_version (route_ver_id, route, dir, valid_during, route_mode) FROM '/data/route_version.csv' CSV HEADER;
